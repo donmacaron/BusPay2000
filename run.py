d6f1ea5ea0783996084f5130cabfa9e4207db113
Don Macaron <donmacaron95@gmail.com>
2022-03-31 20:37:04 +0300

    run py for heroku

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
-import os
 from ipaddress import ip_address
 from BusPay2000 import app
 
 
-port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
-    app.run(host=port, debug=True)
+    app.run(host="0.0.0.0", debug=True)
