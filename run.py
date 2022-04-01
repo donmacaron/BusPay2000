6fc52771a55d5d58d896c31902a1d89a63bef79b
Don Macaron <donmacaron95@gmail.com>
2022-04-01 14:02:44 +0300

    procfile

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+import os
 from ipaddress import ip_address
 from BusPay2000 import app
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 33507)), debug=True)
