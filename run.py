63ae34aa38eda2ff7ed6e8a906a3e111bb5135cf
Don Macaron <donmacaron95@gmail.com>
2022-04-12 18:08:21 +0300

    every deploy recreates db

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import os
 from ipaddress import ip_address
 from BusPay2000 import app
+from BusPay2000 import db
 
 if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 33507)), debug=True)
